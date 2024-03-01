93476e00a43e98bf0a8385452ed07f25a0dd6554
ZenguinM <141882602+ZenguinM@users.noreply.github.com>
2024-03-01 03:25:15 +0000

    V1.1.1

--- aquapon.js
+++ aquapon.js
@@ -4,12 +4,12 @@ function openPara(paraName, elmnt, color) {
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-
     tablink = document.getElementsByClassName("tablink");
     for (i = 0; i < tablink.length; i++) {
         tablink[i].style.backgroundColor = "";
+        tablink[i].style.borderBottom = "2px";
     }
-
     document.getElementById(paraName).style.display = "block";
     elmnt.style.backgroundColor = color;
+    elmnt.style.borderBottom = "0";
 }
