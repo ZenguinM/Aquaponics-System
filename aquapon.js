bf11c3801a16701b678c19397d66f7a03b9a5fbd
ZenguinM <141882602+ZenguinM@users.noreply.github.com>
2024-03-05 04:59:17 +0000

    V1.2.1

--- aquapon.js
+++ aquapon.js
@@ -16,11 +16,13 @@ buttonFish.forEach(fish => {
         output.innerHTML = "Awaiting value...";
         output.style.backgroundColor = "rgb(255,255,255,0.5)";
         desc.innerHTML = `${data.fish[fishId].description}`;
+
     })
 });
 
 buttonPara.forEach(link => {
     link.addEventListener('click', function() {
+        var i, buttonParaNum;
         paraId = this.id;
         info.style.backgroundColor = (window.getComputedStyle(this)).backgroundColor;
         content.style.backgroundColor = (window.getComputedStyle(this)).backgroundColor;
@@ -37,16 +39,26 @@ buttonPara.forEach(link => {
         output.style.display = "block";
         enter.style.display = "block";
 
+        for (i = 0; i < buttonPara.length; i++) {
+            buttonPara[i].style.backgroundColor = "rgb(80,80,80)";
+        };
+
+
         if (paraId == "Temperature") {
             enter.innerHTML = "Enter temperature (°C) below:<br><br>"
+            link.style.backgroundColor = "rgb(128, 0, 0)";
         } else if (paraId == "pH") {
             enter.innerHTML = "Enter pH below:<br><br>";
+            link.style.backgroundColor = "rgb(128, 83, 0)";
         } else if (paraId == "Nitrate") {
             enter.innerHTML = "Enter nitrate level (ppm or mg/L) below:<br><br>";
+            link.style.backgroundColor = "rgb(128, 128, 0)";
         } else if (paraId == "Nitrite") {
             enter.innerHTML = "Enter nitrite level (ppm) below:<br><br>";
+            link.style.backgroundColor = "rgb(0, 128, 0)";
         } else if (paraId == "Ammonia") {
             enter.innerHTML = "Enter ammonia level (ppm) below:<br><br>";
+            link.style.backgroundColor = "rgb(0, 101, 135)";
         }
     });
 })
