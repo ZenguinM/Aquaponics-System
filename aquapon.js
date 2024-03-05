cdf1683dcd82479bf9ca803d7b207760b4c07c13
ZenguinM <141882602+ZenguinM@users.noreply.github.com>
2024-03-05 04:10:07 +0000

    V1.2

--- aquapon.js
+++ aquapon.js
@@ -1,20 +1,31 @@
 import data from "./aquaponvalues.json" assert {type: "json"};
 const info = document.getElementById("info");
+const desc = document.getElementById("descTab");
 const content = document.getElementById("content");
 const enter = document.getElementById("valueEnter");
 const input = document.getElementById("valueInput");
 const output = document.getElementById("valueOutput");
-var paraId;
+var paraId, fishId;
 const buttonPara = document.querySelectorAll('.paralink');
+const buttonFish = document.querySelectorAll('.fishlink');
 
+buttonFish.forEach(fish => {
+    fish.addEventListener('click', function() {
+        fishId = this.id;
+        input.value = "";
+        output.innerHTML = "Awaiting value...";
+        output.style.backgroundColor = "rgb(255,255,255,0.5)";
+        desc.innerHTML = `${data.fish[fishId].description}`;
+    })
+});
 
 buttonPara.forEach(link => {
     link.addEventListener('click', function() {
         paraId = this.id;
         info.style.backgroundColor = (window.getComputedStyle(this)).backgroundColor;
         content.style.backgroundColor = (window.getComputedStyle(this)).backgroundColor;
-        buttonPara.style.backgroundColor = "rgb(80,80,80)";
-        this.style.backgroundColor = (window.getComputedStyle(this)).backgroundColor;
+        //buttonPara.style.backgroundColor = "rgb(80,80,80)";
+        //this.style.backgroundColor = (window.getComputedStyle(this)).backgroundColor;
         info.innerHTML = `<h1>${paraId}</h1>\
         ${data.parameter[paraId].description}<br><br>\
         ${data.parameter[paraId].effect}<br><br>`
@@ -41,76 +52,60 @@ buttonPara.forEach(link => {
 })
 
 input.oninput = function() {
+
     if (input.value == "") {
         output.innerHTML = "Awaiting value..."
         output.style.backgroundColor = "rgb(255,255,255,0.5)"
     } else {
         if (paraId == "Temperature") {
-            if (input.value > data.fish.Barramundi.temperature[1]) {
+            if (input.value > data.fish[fishId].temperature[1]) {
                 output.innerHTML = "The temperature is too high!"
                 output.style.backgroundColor = "rgb(255,100,100)"
-            } else if (input.value < data.fish.Barramundi.temperature[0]) {
+            } else if (input.value < data.fish[fishId].temperature[0]) {
                 output.innerHTML = "The temperature is too low!"
                 output.style.backgroundColor = "rgb(100,100,255)"
             } else {
-                output.innerHTML = "The temperature is ideal for the barramundi. :)"
+                output.innerHTML = `The temperature is ideal for the ${fishId.toLowerCase()}.`
                 output.style.backgroundColor = "rgb(100,255,100)"
             }
         } else if (paraId == "pH") {
-            if (input.value > data.fish.Barramundi.ph[1]) {
+            if (input.value > data.fish[fishId].ph[1]) {
                 output.innerHTML = "The water pH is too high!"
                 output.style.backgroundColor = "rgb(255,100,100)"
-            } else if (input.value < data.fish.Barramundi.ph[0]) {
+            } else if (input.value < data.fish[fishId].ph[0]) {
                 output.innerHTML = "The water pH is too low!"
                 output.style.backgroundColor = "rgb(100,100,255)"
             } else {
-                output.innerHTML = "The water pH is ideal for the barramundi. :)"
+                output.innerHTML = `The water pH is ideal for the ${fishId.toLowerCase()}.`
                 output.style.backgroundColor = "rgb(100,255,100)"
             }
         } else if (paraId == "Nitrate") {
-            if (input.value > data.fish.Barramundi.nitrate) {
+            if (input.value > data.fish[fishId].nitrate[1]) {
                 output.innerHTML = "The nitrate level is too high!"
                 output.style.backgroundColor = "rgb(255,100,100)"
+            } else if (input.value < data.fish[fishId].nitrate[0]) {
+                output.innerHTML = "The nitrate level is too low!"
+                output.style.backgroundColor = "rgb(100,100,255)"
             } else {
-                output.innerHTML = "The nitrate level is ideal for the barramundi. :)"
+                output.innerHTML = `The nitrate level is ideal for the ${fishId.toLowerCase()}.`
                 output.style.backgroundColor = "rgb(100,255,100)"
             }
         } else if (paraId == "Nitrite") {
-            if (input.value > data.fish.Barramundi.nitrite) {
+            if (input.value > data.fish[fishId].nitrite) {
                 output.innerHTML = "The nitrite level is too high!"
                 output.style.backgroundColor = "rgb(255,100,100)"
             } else {
-                output.innerHTML = "The nitrite level is ideal for the barramundi. :)"
+                output.innerHTML = `The nitrite level is ideal for the ${fishId.toLowerCase()}.`
                 output.style.backgroundColor = "rgb(100,255,100)"
             }
         } else if (paraId = "Ammonia") {
-            if (input.value > data.fish.Barramundi.ammonia) {
+            if (input.value > data.fish[fishId].ammonia) {
                 output.innerHTML = "The ammonia level is too high!"
                 output.style.backgroundColor = "rgb(255,100,100)"
             } else {
-                output.innerHTML = "The ammonia level is ideal for the barramundi. :)"
+                output.innerHTML = `The ammonia level is ideal for the ${fishId.toLowerCase()}.`
                 output.style.backgroundColor = "rgb(100,255,100)"
             }
         }
     }
-    
-}
-
-function openPara() {
-
-    /*var i, tabcontent, tablink;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablink = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablink.length; i++) {
-        tablink[i].style.backgroundColor = "";
-        tablink[i].style.borderBottomWidth = "2px";
-    }
-    //fetch("./aquaponvalues.json").then(response => response.json()).then((data) => document.getElementById(paraName).innerHTML = JSON.stringify(data.parameter))
-    document.getElementById(paraName).style.display = "block";
-    elmnt.style.backgroundColor = color;
-    elmnt.style.borderBottomWidth = "0";
-    console.log(paraName);*/
 }
