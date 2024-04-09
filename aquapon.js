19eab43d75f12cf71c1b6be80a68692163b93f82
Taufiq <taufiq.lubis407@gmail.com>
2024-04-09 11:16:40 +0930

    V2.4

--- aquapon.js
+++ aquapon.js
@@ -17,15 +17,16 @@ var customFishData = {};
 const tableData = document.getElementById("customParaTable");
 var selectedFish = document.getElementById("fishSelect");
 var fishName = document.getElementById("fishName");
+const warningError = document.getElementById("warningError");
 
-//Resets values to default
+//Resets multiple input values to default
 function defaultMultiInput() {
     inputMulti[0].value = "25"
     inputMulti[1].value = "7.0"
     inputMulti[2].value = "70"
     inputMulti[3].value = "0"
     inputMulti[4].value = "0"
-    for (i = 0; i < inputMulti.length; i++) {
+    for (var i = 0; i < inputMulti.length; i++) {
         inputMulti[i].style.backgroundColor = "white";
     }
 }
@@ -33,7 +34,6 @@ function defaultMultiInput() {
 //Checks all fish selection buttons and runs the function based on which one is chosen
 buttonFish.forEach(fish => {
     fish.addEventListener('click', function() {
-        var i, j;
         fishId = this.id;
         console.log(fishId)
 
@@ -48,9 +48,20 @@ buttonFish.forEach(fish => {
         output.style.backgroundColor = "rgb(255,255,255,0.5)";
         description.innerHTML = `${data.fish[fishId].description}`;
         addInfo.innerHTML = `${data.fish[fishId].addinfo}`;
+
+        //additional information on temperature is given when pre-installed fish is selected
+        if (paraId == "Temperature") {
+            if (data.fish[fishId].addinfo) {
+                addInfo.style.display = "block";
+            } else {
+                addInfo.style.display = "none";
+            }
+        } else {
+            addInfo.style.display = "none";
+        }
         
         //Iterates through all buttons to change the background colour to grey and text to white
-        for (i = 0; i < buttonFish.length; i++) {
+        for (var i = 0; i < buttonFish.length; i++) {
             buttonFish[i].style.backgroundColor = "rgb(80,80,80)";
             buttonFish[i].style.color = "white";
         };
@@ -66,6 +77,10 @@ buttonFish.forEach(fish => {
 //Checks all parameter selection buttons and runs the function based on which one is chosen
 buttonPara.forEach(link => {
     link.addEventListener('click', function() {
+        if (fishId == undefined) {
+            return
+        }
+
         var i;
         paraId = this.id;
 
@@ -88,11 +103,17 @@ buttonPara.forEach(link => {
         output.innerHTML = "Awaiting value..."
         output.style.backgroundColor = "rgb(255,255,255,0.5)"
 
+        //additional information on temperature is given when pre-installed fish is selected
         if (paraId == "Temperature") {
-            addInfo.style.display = "block";
+            if (data.fish[fishId].addinfo) {
+                addInfo.style.display = "block";
+            } else {
+                addInfo.style.display = "none";
+            }
         } else {
             addInfo.style.display = "none";
         }
+
         input.style.display = "block";
         output.style.display = "block";
         enter.style.display = "block";
@@ -102,7 +123,7 @@ buttonPara.forEach(link => {
             buttonPara[i].style.backgroundColor = "rgb(80,80,80)";
         };
 
-        //Repeat if statements check the id of the button to determine which button should change colour to indicate selection
+        //Repeated if statements check the id of the button to determine which button should change colour to indicate selection
         if (paraId == "Temperature") {
             enter.innerHTML = "Enter temperature (°C) below:<br><br>"
             content.style.backgroundColor = "rgb(128, 0, 0)";
@@ -132,7 +153,7 @@ buttonPara.forEach(link => {
 
 //Updates the output if the input is changed
 input.oninput = function() {
-    //A backup function is ran if there is no value
+    //A backup function is ran if there is no value and if a fish is not selected yet
     if (input.value == "") {
         output.innerHTML = "Awaiting value..."
         output.style.backgroundColor = "rgb(255,255,255,0.5)"
@@ -143,8 +164,7 @@ input.oninput = function() {
         /*Checks the id of the button and fish selected, and compares the input value to the corresponding values in the .json file
             Adjust style accordingly
             Note to account for ranges of values, such as temp and pH where there is an upper and lower bound, unlike ammonia and nitrite,
-            an array with 0 at the 0th index is placed in the JSON file (all values should be positive, and negative temp = ice)
-            the value at the 1st index will account for the maximum value. */   
+            an array with 0 at the 0th index is placed in the JSON file (all values should be positive, and negative temp = ice)*/   
         if (input.value > data.fish[fishId][paraId][1]) {
             output.innerHTML = `The ${paraId.toLowerCase()} is too high!<br><br>\
             Suggestion: ${data.parameter[paraId].decreasing}`;
@@ -152,7 +172,7 @@ input.oninput = function() {
         } else if (input.value < data.fish[fishId][paraId][0]) {
             output.innerHTML = `The ${paraId.toLowerCase()} is too low!<br><br>\
             Suggestion: ${data.parameter[paraId].increasing}`
-            output.style.backgroundColor = "rgb(161,211,255)"
+            output.style.backgroundColor = "rgb(110, 165, 227)"
         } else {
             output.innerHTML = `The ${paraId.toLowerCase()} is ideal for the ${fishId.toLowerCase()}.`
             output.style.backgroundColor = "rgb(138,252,136)"
@@ -169,7 +189,7 @@ inputMulti.forEach(inputMultiPara => {
             if (inputMulti[i].value > data.fish[fishId][parameter[i]][1]) {
                 inputMulti[i].style.backgroundColor = "rgb(255,140,128)";
             } else if (inputMulti[i].value < data.fish[fishId][parameter[i]][0]) {
-                inputMulti[i].style.backgroundColor = "rgb(161,211,255)";
+                inputMulti[i].style.backgroundColor = "rgb(110, 165, 227)";
             } else {
                 inputMulti[i].style.backgroundColor = "rgb(138,252,136)";
             }
@@ -179,8 +199,22 @@ inputMulti.forEach(inputMultiPara => {
 
 //Function will collect table data and organise it into an object for JSON
 submitButton.addEventListener('click', function() {
+    //Checks name input for empty value, returns if true
     if (fishName.value == "") {
+        warningError.style.display = "block"
+        warningError.innerHTML = "Error: No name entered!<br><br>Please enter a fish name in the input above the table!"
         return
+    }
+
+    //Checks parameter value pairs, returns if maximum is less than minimum
+    for (var i = 1; i < 4; i++) {
+        var min = parseFloat(tableData.rows[1].cells[i].children[0].value)
+        var max = parseFloat(tableData.rows[2].cells[i].children[0].value)
+        if (min > max) {
+            warningError.style.display = "block"
+            warningError.innerHTML = "Error: Maximum and minimum values not appropriate!<br><br>Please check that the maximum values are not less than the minimum values (only for temperature, pH, and nitrate)!"
+            return
+        }
     }
 
     var newOpt = document.createElement('option');
@@ -200,7 +234,14 @@ submitButton.addEventListener('click', function() {
         //Seperate loop for first row as there are NA cells
         if (i == 1) {
             for (var j = 1; j < 4; j++) {
-                customFishData[parameter[j-1]].push(parseFloat(tableRow.cells[j].children[0].value));
+                //Checks for negative values, returns if present
+                if (parseFloat(tableRow.cells[j].children[0].value) < 0) {
+                    warningError.style.display = "block"
+                    warningError.innerHTML = "Error: Negative value detected!<br><br>Please remove the negative sign from the corresponding cell in the table above!"
+                    return
+                } else {
+                    customFishData[parameter[j-1]].push(parseFloat(tableRow.cells[j].children[0].value));
+                }
             }
             //Minimum value for nitrite and ammonia is zero
             customFishData['Nitrite'].push(0);
@@ -208,9 +249,15 @@ submitButton.addEventListener('click', function() {
         }
         if (i == 2) {
             for (var j = 1; j < 6; j++) {
-                customFishData[parameter[j-1]].push(parseFloat(tableRow.cells[j].children[0].value));
+                if (parseFloat(tableRow.cells[j].children[0].value) < 0) {
+                    warningError.style.display = "block"
+                    warningError.innerHTML = "Error: Negative value detected!<br><br>Please remove the negative sign from the corresponding cell in the table above!"
+                    return
+                } else {
+                    customFishData[parameter[j-1]].push(parseFloat(tableRow.cells[j].children[0].value));
+                }
             }
-        }
+        } 
     }
 
     //Checks if the fish in the name input already exists
@@ -242,6 +289,7 @@ submitButton.addEventListener('click', function() {
     console.log(data.fish);
     fishId = selectedFish.value;
     console.log(fishId);
+    warningError.style.display = "none"   
 })
 
 //Changes name input and table values when selected
